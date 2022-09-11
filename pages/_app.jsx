4ad5097fa0c80997b7eb631ce539ed78a8a18eaf
Busayo Samuel <busayosamuel2016@gmail.com>
2022-09-11 10:53:32 +0100

    resolved typescript deployment error

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,7 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { RecoilRoot } from "recoil";
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <RecoilRoot>
